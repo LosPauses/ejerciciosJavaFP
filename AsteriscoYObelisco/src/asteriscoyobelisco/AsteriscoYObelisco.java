63ef6d2dfffd3dd20929a758c29411c3340a2d34
LosPauses <32800783+LosPauses@users.noreply.github.com>
2017-12-03 01:08:28 +0100

    Actualizado
    HE AÑADIDO LAS ULTIMAS FUNCIONES DE LA PRIMERA PARTE DEL EJERCICIO DE CLASE DE LOS TRIANGULOS DE ASTERISCOS DE MIERDA DE AYER DE MANERA FURIOSA PQ SON MAYUSCULAS. voy a dormir, me qedo agusto de aber contivuido al softguar livre

--- AsteriscoYObelisco/src/asteriscoyobelisco/AsteriscoYObelisco.java
+++ AsteriscoYObelisco/src/asteriscoyobelisco/AsteriscoYObelisco.java
@@ -1,7 +1,5 @@
 package asteriscoyobelisco;
 
-import java.util.Scanner;
-
 public class AsteriscoYObelisco {
 
     public static void main(String[] args) {
@@ -14,7 +12,7 @@ public class AsteriscoYObelisco {
         maxCol = 1;
         base = 4;
 
-        triangulo2(base);
+        triangulo3v2(base);
     }
 
     private static void cuadrado(int maxRow, int maxCol) {
@@ -26,9 +24,23 @@ public class AsteriscoYObelisco {
         }
     }
 
-    private static void triangulo(int base) {
+    private static void triangulo1(int base) {
         for (int row = 0; row < base; row++) {
             for (int col = 0; col <= row; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void triangulo1v2(int base) {
+        for (int row = 0; row < base; row++) {
+            int col = 0;
+
+            for (; col < (base - 1) - row; col++) {
+                System.out.print("");
+            }
+            for (; col < base; col++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -41,6 +53,43 @@ public class AsteriscoYObelisco {
 
             for (; col < (base - 1) - row; col++) {
                 System.out.print("-");
+            }
+            for (; col < base; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void triangulo3(int base) {
+        for (int row = base; row > 0; row--) {
+            for (int col = 0; col < row; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void triangulo3v2(int base) {
+        for (int row = base; row > 0; row--) {
+            int col = 0;
+
+            for (; col < (base) - row; col++) {
+                System.out.print("");
+            }
+            for (; col < base; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void triangulo4(int base) {
+        for (int row = base; row > 0; row--) {
+            int col = 0;
+
+            for (; col < (base) - row; col++) {
+                System.out.print(" ");
             }
             for (; col < base; col++) {
                 System.out.print("*");
